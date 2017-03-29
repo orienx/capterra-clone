c26e8ad873371ecc7361a6a4872c0077b1a8cc9f
Nathan Charrois <ncharrois@deversus.com>
2017-03-29 11:43:22 -0700

    Test Commit

--- resources/views/layout/public.blade.php
+++ resources/views/layout/public.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="description" content="for architecture and urban enthusiasts.">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Capterra Clone</title>
+  <title>Capterra Clone!</title>
 </head>
 <body>
   @yield('content')
